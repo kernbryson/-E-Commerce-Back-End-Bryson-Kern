23b61cf6a1398768d1270ec71720fb3e26882d47
Bryson Kern <kernbryson@yahoo.com>
2022-05-28 13:53:40 -0400

    created post route for Tags

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -24,10 +24,40 @@ router.get("/", (req, res) => {
 router.get("/:id", (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  Tag.findOne({
+    where: {
+      id: req.params.id,
+    },
+    attributes: ["id", "tag_name"],
+    include: [
+      {
+        model: Product,
+        attributes: ["product_name", "price", "stock", "category_id"],
+      },
+    ],
+  })
+    .then((singleTag) => {
+      if (!singleTag) {
+        res.status(404).json({ message: "No product found!" });
+        return;
+      }
+      res.json(singleTag);
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
 });
 
 router.post("/", (req, res) => {
   // create a new tag
+  Tag.create({
+    tag_name: req.body.tag_name
+  })
+  .then((newTag) => res.status(200).json(newTag))
+  .catch((err) => {
+    console.log(err);
+    res.status(400).json(err);
+  });
 });
 
 router.put("/:id", (req, res) => {
